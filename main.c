e932d71d080161c2e9c9eaee737030eb2bcfbb84
Ana <aninhaass99@gmail.com>
2024-11-19 19:43:51 -0300

    ......

--- main.c
+++ main.c
@@ -1,41 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int t;
-    int *vetor;
-
+#include<stdio.h>
     
-    scanf("%d", &t);
-
-    if (t <= 0) {
-        printf("[vazio]\n");
-        return 0;
-    }
-
+    int main()
+    {
+     int *vetor;
+     int t;
+     
+       scanf("%d",&t);
+       
+       vetor = (int *)malloc(t*sizeof(int));
+       
+       if(t == 0){
+       printf("[vazio]\n");
+       }
+       
+       
+       
+       for( int i = 0; i < t; i++){
+         scanf("%d", &vetor[i]);
+       }
+       
+       printf("[");
+       for( int i = 0; i < t; i++){
+       printf("%d", vetor[i]);
+       if(i < t - 1){
+       printf(",");
+       }
+       }
     
-    vetor = (int *)malloc(t * sizeof(int));
-    if (vetor == NULL) {
-        return 1;
-    }
-
+      printf("]\n");
+      
+      free(vetor);
     
-    for (int i = 0; i < t; i++) {
-        scanf("%d", &vetor[i]);
-    }
-
     
-    printf("[");
-    for (int i = 0; i < t; i++) {
-        printf("%d", vetor[i]);
-        if (i < t - 1) {
-            printf(",");
-        }
-    }
-    printf("]\n");
-
     
-    free(vetor);
-
+    
     return 0;
-}
+    }
