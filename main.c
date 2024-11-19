012a23a3697334c8076b574d2f00bcb724a095fe
Ana <aninhaass99@gmail.com>
2024-11-19 20:02:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,37 +11,31 @@
        
        vetor = (int *)malloc(t*sizeof(int));
        
-       if(t == 0){
-       printf("[vazio]");
-       }
-       
-       
-       
-       for( int i = 0; i < t; i++){
+        for( int i = 0; i < t; i++){
          scanf("%d", &vetor[i]);
        }
-       
-       printf("[");
-       for( int i = 0; i < t; i++){
-
-           if(i==t-1){
-
-               printf("%d", vetor[i]);
-               
-           }else{
-
-               printf("%d, ", vetor[i]);
-
-           }
-     
-       }
-    
-      printf("]\n");
-      
+       if(t == 0){
+           printf("[vazio]");
+       }else{
+       	
+	       printf("[");
+	       for( int i = 0; i < t; i++){
+	
+	           if(i==t-1){
+	
+	               printf("%d", vetor[i]);
+	               
+	           }else{
+	
+	               printf("%d, ", vetor[i]);
+	
+	           }
+	     
+	       }
+	    
+	      printf("]\n");
+  		}
       free(vetor);
-    
-    
-    
-    
+ 
     return 0;
     }
